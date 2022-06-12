fc2f6b375b8a00a56072c0dc3ef5931cd99c4cb4
Marcoscappelli <cappelli.m@gmail.com>
2022-06-12 19:14:36 -0300

    CSS global e module

--- reactcosts/src/App.js
+++ reactcosts/src/App.js
@@ -1,25 +1,30 @@
-import logo from './logo.svg';
+import HelloWorld from './components/HelloWorld';
 import './App.css';
+import Pessoa from './components/Pessoa';
+import SayMyName from './components/SayMyName';
+import Frase from './components/Frase';
 
 function App() {
+  const nome = "Maria joaquina"
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Teste CSS global</h1>
+      <Frase />
+      <Frase />
+      <HelloWorld />
+      <SayMyName nome= "Marcolino"/>
+      <SayMyName nome= "Marcelo"/>
+      <SayMyName nome= {nome} />
+      <Pessoa
+        nome= "Rogério"
+        idade= "45"
+        profissao= "Motorista app"
+        foto= "https://via.placeholder.com/150"
+        />
+
     </div>
-  );
+  )
 }
 
 export default App;
