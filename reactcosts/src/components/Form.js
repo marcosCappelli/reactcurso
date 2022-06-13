f143617191aeefc0e28581b585c68cab1e430eb6
Marcoscappelli <cappelli.m@gmail.com>
2022-06-13 17:44:35 -0300

    commit useState hook

--- reactcosts/src/components/Form.js
+++ reactcosts/src/components/Form.js
@@ -1,17 +1,42 @@
+
+import { useState } from "react"
+
 function Form() {
 
     function cadastrarUsuario(event) {
         //Usado esse event para evitar envio "submit"
         event.preventDefault()
+        console.log(name)
         console.log("Cadastrou o osuário!")
+        console.log(`Usuário ${name} foi cadastrado com a senha: ${password}`)
     }
+
+    const [name, setName] = useState()
+    const [password, setPassword] = useState()
 
     return (
         <div>
             <h1>Meu cadastro</h1>
             <form onSubmit={cadastrarUsuario}>
                 <div>
-                    <input type="text" placeholder="Digite o seu nome" />
+                    <label htmlFor="name">Nome:</label>
+                    <input 
+                    type="text" 
+                    id="name" 
+                    naem="name"
+                     placeholder="Digite o seu nome" 
+                     onChange={(e) => setName(e.target.value)}
+                     />
+                </div>
+                <div>
+                    <label htmlFor="password">Senha:</label>
+                    <input
+                     type="password" 
+                     id="password" 
+                     name="password"
+                      placeholder="Digite a sua senha" 
+                      onChange={(e) => setPassword(e.target.value)}
+                      />
                 </div>
                 <div>
                     <input type="submit" value="Cadastrar" />
