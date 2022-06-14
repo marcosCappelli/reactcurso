c08b8dad8f22938de82438771ee3d5c4f2d31fce
Marcoscappelli <cappelli.m@gmail.com>
2022-06-14 17:16:04 -0300

    renderização de Lista

--- reactcosts/src/App.js
+++ reactcosts/src/App.js
@@ -1,4 +1,28 @@
+import {useState} from 'react'
+
 import './App.css';
+import Saudacao from './components/Saudacao';
+import SeuNome from './components/SeuNome';
+
+function App() {
+
+  const [nome, setNome] = useState()
+
+  return (
+    <div className="App">
+      <h1>State Lift</h1>
+      <SeuNome setNome={setNome} />
+      <Saudacao nome={nome}/>
+    </div>
+  )
+}
+
+export default App
+
+
+
+
+/*
 import OutraLista from './components/OutraLista';
 
 function App() {
@@ -15,12 +39,7 @@ function App() {
 }
 
 export default App
-
-
-
-
-
-
+*/
 
 /*import Condicional from './components/Condicional';
 
